64fc86a693eb881f18be37e644dc7e2b3888ebe9
Liech <nicolas_harttig@hotmail.de>
2025-10-11 13:20:47 +0200

    assimps bad. from scratch examples to save triangulation crashs

--- Executable/Source/main.cpp
+++ Executable/Source/main.cpp
@@ -2,28 +2,17 @@
 
 #include "Library/Util/LoadFileDialog.h"
 #include "Library/Util/SaveFileDialog.h"
-#include "Library/Triangle/AssimpAsset.h"
 #include "Library/Triangle/Triangulation.h"
 
 int main()
 {
-    Library::LoadFileDialog dlg;
-    dlg.addFilter("assimp asset files",Library::AssimpAsset::getSupportedFormats());
+      Library::LoadFileDialog dlg;
     dlg.addFilter("all files", { "*" });
 
     dlg.execute();
     if (!dlg.isCancled())
     {
         std::cout << "file: " << dlg.getResultPath() << std::endl;
-        auto asset = Library::AssimpAsset::load(dlg.getResultPath());
-        //Library::SaveFileDialog sdlg;
-        //sdlg.addFilter("assimp asset files", Library::AssimpAsset::getSupportedFormats());
-        //sdlg.execute();
-        //if (!sdlg.isCancled())
-        //{
-            //asset->save(sdlg.getResultPath());
-        asset->toTriangulation()->saveAsSTL(dlg.getResultPath() + ".stl");
-        //}
     }
     std::cout << dlg.getResultPath();
 }
