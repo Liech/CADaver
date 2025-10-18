94843dcc2daebaebffcc54cf21aeb1400578505d
Liech <nicolas_harttig@hotmail.de>
2025-10-18 13:13:44 +0200

    added convert button

--- godotProject/UI/Content/MenuBar/menu_bar_file.gd
+++ godotProject/UI/Content/MenuBar/menu_bar_file.gd
@@ -2,13 +2,13 @@ class_name FileMenu extends PopupMenu
 
 @export var bar : ApplicationMenuBar;
 
-var index_new  = 0;
-var index_load = 1;
-var index_save = 2;
-var index_quit = 3;
+var index_new     = 0;
+var index_load    = 1;
+var index_convert = 2;
+var index_save    = 3;
+var index_quit    = 4;
 
 var continue_closing := false
-
 
 func _ready() -> void:
 	Hub.file.drawings_changed.connect(on_drawings_changed);
@@ -16,6 +16,7 @@ func _ready() -> void:
 
 func on_drawings_changed() -> void:
 	set_item_disabled(index_save,Hub.file.drawings.size() == 0)
+	set_item_disabled(index_convert,Hub.file.drawings.size() == 0)
 
 func _on_index_pressed(index: int) -> void:
 	match index:
@@ -23,6 +24,8 @@ func _on_index_pressed(index: int) -> void:
 			new_pressed();
 		index_load:
 			load_pressed();
+		index_convert:
+			convert_pressed();
 		index_save:
 			save_pressed();
 		index_quit:
@@ -36,6 +39,9 @@ func load_pressed():
 	
 func save_pressed():
 	invokeSaveFileDialog(bar.window.scene.drawing)
+
+func convert_pressed():
+	pass
 
 func new_drawing() -> void:
 	var newOne := Drawing.new()
